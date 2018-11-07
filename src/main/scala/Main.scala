a072d99bbc9d576d951e5eae1f11035dbf3cc0b3
Uriel Espinoza <urielintemperie@gmail.com>
2018-11-07 13:56:09 -0300

    Trabajo hasta presentacion 2018-11-06

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,28 +1,38 @@
-
-case class Numero(var num : Int) extends Expresion
-
+package Lenguaje
 abstract class Expresion{
 }
 
-case class Suma(numero1 : Numero, numero2 : Numero) extends Expresion
+trait Evaluable extends Expresion
 
-case class Resta(numero1 : Numero, numero2 : Numero) extends Expresion
+case class Numero(var num : Int) extends Expresion with Evaluable
 
-case class Division(numero1 : Numero, numero2 : Numero) extends Expresion
+trait Booleano extends Expresion with Evaluable  {
+}
 
-case class Multiplicacion(numero1 : Numero, numero2 : Numero) extends Expresion
+case class True() extends Booleano
 
-case class Mayor(numero1 : Numero, numero2 : Numero) extends Expresion
+case class False() extends Booleano
 
-case class Menor(numero1 : Numero, numero2 : Numero) extends Expresion
 
-case class Igual(numero1 : Numero, numero2 : Numero) extends Expresion
+case class Suma(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
 
-case class Distinto(numero1 : Numero, numero2 : Numero) extends Expresion
+case class Resta(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
 
-case class MenorOIgual(numero1 : Numero, numero2 : Numero) extends Expresion
+case class Division(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
 
-case class MayorOIgual(numero1 : Numero, numero2 : Numero) extends Expresion
+case class Multiplicacion(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
+
+case class Mayor(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
+
+case class Menor(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
+
+case class Igual(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
+
+case class Distinto(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
+
+case class MenorOIgual(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
+
+case class MayorOIgual(numero1 : Evaluable, numero2 : Evaluable) extends Expresion
 
 
 case class Programa(var expresion : Expresion)
@@ -36,12 +46,48 @@ case class Advertencia() extends Gravedad
 
 case class Problema(var descripcion : String, var gravedad : Gravedad, var expresion : Expresion)
 
-abstract class Regla(){
-  def evaluar(expresion : Expresion): Option[Problema]
+abstract class Regla() {
+  def evaluar(expresion: Expresion): Option[Problema]
+
+  def listIfReferece(evaluable: Evaluable): List[Referencia] = {
+    evaluable match {
+      case r@Referencia(a) => List(r)
+      case _ => List()
+    }
+  }
+
+  def extraerReferencias(expresion: Expresion): List[Referencia] = {
+    expresion match {
+      case Suma(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case Resta(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case Division(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case Multiplicacion(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case Mayor(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case Menor(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case Igual(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case Distinto(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case MayorOIgual(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+
+      case MenorOIgual(a, b) => listIfReferece(a) ++ listIfReferece(b) ++ List()
+    }
+  }
 }
 
-class DivisionPor0 extends Regla{
-  override def evaluar(expresion : Expresion): Option[Problema] = {
+
+
+
+
+
+class DivisionPor0 extends Regla {
+  override def evaluar(expresion: Expresion): Option[Problema] = {
     expresion match {
       case Division(_, Numero(0)) => Some(Problema("no se puede dividir por cero", Error(), expresion))
       case _ => None
@@ -49,8 +95,8 @@ class DivisionPor0 extends Regla{
   }
 }
 
-class OperacionRedundante extends Regla{
-  override def evaluar(expresion : Expresion): Option[Problema] = {
+class OperacionRedundante extends Regla {
+  override def evaluar(expresion: Expresion): Option[Problema] = {
     expresion match {
       case Resta(_, Numero(0)) | Suma(_, Numero(0)) | Multiplicacion(_, Numero(1)) | Division(_, Numero(1)) => Some(Problema("operacion redundante", Advertencia(), expresion))
       case _ => None
@@ -58,17 +104,74 @@ class OperacionRedundante extends Regla{
   }
 }
 
-class ComparacionSinSentido extends Regla{
-  override def evaluar(expresion : Expresion): Option[Problema] = {
+class ComparacionSinSentido extends Regla {
+  override def evaluar(expresion: Expresion): Option[Problema] = {
     expresion match {
-      case Igual(Numero(2), Numero(2)) | Menor(Numero(3), Numero(4)) | Mayor(Numero(3), Numero(4)) => Some(Problema("comparacion sin sentido", Advertencia(), expresion))
+      case Igual(Numero(_), Numero(_)) | Menor(Numero(_), Numero(_)) | Mayor(Numero(_), Numero(_)) => Some(Problema("comparacion sin sentido", Advertencia(), expresion))
       case _ => None
     }
   }
 }
 
-class Chequeador(val prog : Programa, val _reglas : List[Regla]){
-  var reglas : List[Regla] = _reglas
+class VariableDuplicada extends Regla {
+  override def evaluar(expresion: Expresion): Option[Problema] = {
+    if (Referencias.varDuplicated.nonEmpty) {
+      Some(Problema("Variables Duplicadas", Error(), Referencias.varDuplicated.head))
+    } else {
+      None
+    }
+  }
+}
+
+class VariableNoDeclarada extends Regla{
+  override def evaluar(expresion: Expresion): Option[Problema] = {
+    //println(this.extraerReferencias(expresion))
+    var referenciasNoDeclaradas = this.extraerReferencias(expresion).filter(r => Referencias.getVarOption(r.clave).isEmpty) //TODO revisar
+    referenciasNoDeclaradas match {
+      case r :: ls => Some(Problema("Variables no declarada", Error(), r))
+      case Nil => None
+    }
+  }
+}
+
+class VariableDeclaradaNoUsada extends Regla{
+  override def evaluar(expresion: Expresion): Option[Problema] = {
+    var referencias = this.extraerReferencias(expresion)
+
+    var variablesUsadas:List[Variable]= List()
+
+    referencias.foreach(r => Referencias.getVarOption(r.clave).foreach(variable => variablesUsadas = variable :: variablesUsadas))
+
+    //println(variablesUsadas.head.clave)
+    var variablesNoUsadas = Referencias.referencias.values.toList.filter(v => !variablesUsadas.contains(v))
+
+    //println(variablesNoUsadas.head.clave)
+
+    variablesNoUsadas match {
+      case r :: ls => Some(Problema("Variables declaradas no usadas", Advertencia(), r))
+      case Nil => None
+    }
+  }
+}
+
+class VariableUsadaNoAsignada extends Regla{
+  override def evaluar(expresion: Expresion): Option[Problema] = {
+    var referencias = this.extraerReferencias(expresion)
+    var variablesUsadas:List[Variable]= List()
+
+    referencias.foreach(r => Referencias.getVarOption(r.clave).foreach(variable => variablesUsadas = variable :: variablesUsadas))
+    variablesUsadas.filter(variable => variable.valor == None)
+
+    variablesUsadas match {
+      case r :: ls => Some(Problema("Variables usadas no asignadas", Error(), r))
+      case Nil => None
+    }
+  }
+}
+
+
+class Chequeador(val prog: Programa, val _reglas: List[Regla]) {
+
 
   def chequear(): List[Problema] = {
     var problemas = List[Problema]()
@@ -77,85 +180,123 @@ class Chequeador(val prog : Programa, val _reglas : List[Regla]){
   }
 }
 
-trait Booleano extends Expresion{
-}
+  class Refactorizador() {
+    def boolToBooleano(boolean : Boolean): Booleano ={
+      boolean match {
+        case true => True()
+        case false => False()
+      }
+    }
 
-case class True() extends Booleano
-
-case class False() extends Booleano
-
-class Refactorizador(){
-  def refactorizar(prog : Programa): Programa = {
-    prog.expresion match {
-      case Suma(n, Numero(0)) => Programa(n)
-      case Resta(n, Numero(0)) => Programa(n)
+    def refactorizar(prog: Programa): Programa = {
+      prog.expresion match {
+        case Suma(n, Numero(0)) => Programa(n)
+        case Resta(n, Numero(0)) => Programa(n)
+        case Multiplicacion(n, Numero(1)) => Programa(n)
+        case Division(n, Numero(1)) => Programa(n)
+        case Mayor(Numero(n), Numero(m)) => Programa(boolToBooleano(n>m))
+        case Menor(Numero(n), Numero(m)) => Programa(boolToBooleano(n<m))
+        case Igual(Numero(n), Numero(m)) => Programa(boolToBooleano(n==m))
+        case Distinto(Numero(n), Numero(m)) => Programa(boolToBooleano(n!=m))
+        case MenorOIgual(Numero(n), Numero(m)) => Programa(boolToBooleano(n<=m))
+        case MayorOIgual(Numero(n), Numero(m)) => Programa(boolToBooleano(n>=m))
+        case _ => prog
+      }
     }
   }
-}
 
-class Interprete(){
+  class Interprete() {
 
-  def mayor(a : Int, b : Int): Booleano ={
-    var rta : Booleano = False()
-    if(a > b){
-      rta = True()
+    def mayor(a: Int, b: Int): Booleano = {
+      var rta: Booleano = False()
+      if (a > b) {
+        rta = True()
+      }
+      return rta
     }
-    return rta
-  }
 
-  def menor(a : Int, b : Int): Booleano = {
-    var rta : Booleano = False()
-    if(a < b){
-      rta = True()
+    def menor(a: Int, b: Int): Booleano = {
+      var rta: Booleano = False()
+      if (a < b) {
+        rta = True()
+      }
+      return rta
     }
-    return rta
-  }
 
-  def igual(a : Int, b : Int): Booleano = {
-    var rta : Booleano = False()
-    if(a == b){
-      rta = True()
+    def igual(a: Int, b: Int): Booleano = {
+      var rta: Booleano = False()
+      if (a == b) {
+        rta = True()
+      }
+      return rta
     }
-    return rta
-  }
 
-  def distinto(a : Int, b : Int): Booleano = {
-    var rta : Booleano = False()
-    if(a != b){
-      rta = True()
+    def distinto(a: Int, b: Int): Booleano = {
+      var rta: Booleano = False()
+      if (a != b) {
+        rta = True()
+      }
+      return rta
     }
-    return rta
-  }
 
-  def mayorOIgual(a: Int, b: Int): Booleano = {
-    var rta : Booleano = False()
-    if(a >= b){
-      rta = True()
+    def mayorOIgual(a: Int, b: Int): Booleano = {
+      var rta: Booleano = False()
+      if (a >= b) {
+        rta = True()
+      }
+      return rta
     }
-    return rta
-  }
 
-  def menorOIgual(a: Int, b: Int): Booleano = {
-    var rta : Booleano = False()
-    if(a <= b){
-      rta = True()
+    def menorOIgual(a: Int, b: Int): Booleano = {
+      var rta: Booleano = False()
+      if (a <= b) {
+        rta = True()
+      }
+      return rta
     }
-    return rta
-  }
 
-  def ejecutar(prog : Programa): Expresion = {
-    prog.expresion match {
-      case Suma(Numero(a), Numero(b)) => Numero(a+b)
-      case Resta(Numero(a), Numero(b)) => Numero(a-b)
-      case Division(Numero(a), Numero(b)) => Numero(a / b)
-      case Multiplicacion(Numero(a), Numero(b)) => Numero(a*b)
-      case Mayor(Numero(a), Numero(b)) => mayor(a,b)
-      case Menor(Numero(a), Numero(b)) => menor(a,b)
-      case Igual(Numero(a), Numero(b)) => igual(a,b)
-      case Distinto(Numero(a), Numero(b)) => distinto(a,b)
-      case MayorOIgual(Numero(a), Numero(b)) => mayorOIgual(a,b)
-      case MenorOIgual(Numero(a), Numero(b)) => menorOIgual(a,b)
+
+    def evaluar(exp: Evaluable): Evaluable = {
+      exp match {
+        case Numero(a) => Numero(a)
+        case a: Booleano => a
+        case r1@Referencia(a) => r1.getValue()
+      }
+    }
+
+    def ejecutar(prog: Programa): Expresion = {
+      prog.expresion match {
+        case Suma(Numero(a), Numero(b)) => Numero(a + b)
+        case Suma(a, b) => this.ejecutar(Programa(Suma(this.evaluar(a), this.evaluar(b))))
+
+        case Resta(Numero(a), Numero(b)) => Numero(a - b)
+        case Resta(a, b) => this.ejecutar(Programa(Resta(this.evaluar(a), this.evaluar(b))))
+
+        case Division(Numero(a), Numero(b)) => Numero(a / b)
+        case Division(a, b) => this.ejecutar(Programa(Division(this.evaluar(a), this.evaluar(b))))
+
+        case Multiplicacion(Numero(a), Numero(b)) => Numero(a * b)
+        case Multiplicacion(a, b) => this.ejecutar(Programa(Multiplicacion(this.evaluar(a), this.evaluar(b))))
+
+        case Mayor(Numero(a), Numero(b)) => mayor(a, b)
+        case Mayor(a, b) => this.ejecutar(Programa(Mayor(this.evaluar(a), this.evaluar(b))))
+
+        case Menor(Numero(a), Numero(b)) => menor(a, b)
+        case Menor(a, b) => this.ejecutar(Programa(Menor(this.evaluar(a), this.evaluar(b))))
+
+        case Igual(Numero(a), Numero(b)) => igual(a, b)
+        case Igual(a, b) => this.ejecutar(Programa(Igual(this.evaluar(a), this.evaluar(b))))
+
+        case Distinto(Numero(a), Numero(b)) => distinto(a, b)
+        case Distinto(a, b) => this.ejecutar(Programa(Distinto(this.evaluar(a), this.evaluar(b))))
+
+        case MayorOIgual(Numero(a), Numero(b)) => mayorOIgual(a, b)
+        case MayorOIgual(a, b) => this.ejecutar(Programa(MayorOIgual(this.evaluar(a), this.evaluar(b))))
+
+        case MenorOIgual(Numero(a), Numero(b)) => menorOIgual(a, b)
+        case MenorOIgual(a, b) => this.ejecutar(Programa(MenorOIgual(this.evaluar(a), this.evaluar(b))))
+
+      }
     }
   }
-}
 
--- src/test/scala/GeneralTest.scala
+++ src/test/scala/GeneralTest.scala
@@ -1,7 +1,9 @@
+package Lenguaje
 import org.scalatest.FlatSpec
 
 class GeneralTest extends FlatSpec {
   //--------------------------Test parte 1--------------------------
+
 
   "Un programa" should "poder tener una expresion" in {
     val primerNumero = Numero(1)
@@ -25,7 +27,7 @@ class GeneralTest extends FlatSpec {
     assert(chequeador.chequear == List(problemaDivisionPor0))
   }
 
-  "un refactoriador" should "recibir una operacion redundante y refactorizarla" in {
+  "un refactoriador" should "recibir una operacion de Suma redundante y refactorizarla" in {
     val primerNumero = Numero(4)
     val segundoNumero = Numero(0)
     val operacionSuma = Suma(primerNumero, segundoNumero)
@@ -35,6 +37,39 @@ class GeneralTest extends FlatSpec {
 
     assert(refactorizador.refactorizar(programa) == Programa(Numero(4)))
 
+  }
+
+  "un refactorizador" should "recibir una operacion de Multiplicacion redundante y refactorizarla" in {
+    val primerNumero = Numero(3)
+    val segundoNumero = Numero(1)
+    val operacionMultiplicacion = Multiplicacion(primerNumero, segundoNumero)
+    val programa = Programa(operacionMultiplicacion)
+
+    val refactorizador = new Refactorizador()
+
+    assert(refactorizador.refactorizar(programa) == Programa(Numero(3)))
+  }
+
+  "un refactorizador" should "recibir una comparacion Mayor sin sentido y refactorizarla" in {
+    val primerNumero = Numero(3)
+    val segundoNumero = Numero(2)
+    val comparacionMayor = Mayor(primerNumero, segundoNumero)
+    val programa = Programa(comparacionMayor)
+
+    val refactorizador = new Refactorizador()
+
+    assert(refactorizador.refactorizar(programa) == Programa(True()))
+  }
+
+  "un refactorizador" should "recibir una comparacion Menor sin sentido y refactorizarla" in {
+    val primerNumero = Numero(4)
+    val segundoNumero = Numero(3)
+    val comparacionMenor = Menor(primerNumero, segundoNumero)
+    val programa = Programa(comparacionMenor)
+
+    val refactorizador = new Refactorizador()
+
+    assert(refactorizador.refactorizar(programa) == Programa(False()))
   }
 
   "un interprete" should "recibir un programa y devolver su valor" in {
@@ -47,4 +82,139 @@ class GeneralTest extends FlatSpec {
 
     assert(interprete.ejecutar(programa) == Numero(5))
   }
+
+
+  "una variable" should "poder crearla y referenciarla" in {
+    val variable1 = Variable("anioActual", Numero(2018))
+    val variable2 = Variable("edad", Numero(25))
+    //val programa = Programa(Resta(Referencia("anioActual"), Referencia("edad"))
+    //val interprete = new Interprete()
+
+    assert(Referencias.getVar("anioActual") == variable1)
+    assert(Referencias.getVar("edad") != variable1)
+    assert(Referencias.getVar("edad") == variable2)
+  }
+
+  /*
+  "una variable" should "poder hacer una referecia con referencia" in {
+    val variable1 = Variable("anioActual", Numero(2018))
+    val variable2 = Variable("edad", Numero(25))
+    val programa = Programa(Resta(Referencia("anioActual"), Referencia("edad"))
+    val interprete = new Interprete()
+
+    assert(Referencia("anioActual") == variable1)
+  }
+*/
+
+  "un interprete" should "recibir un programa con referencias y devolver su valor" in {
+    val primerNumero = Numero(4)
+    val variable1 = Variable("segundoValor", Numero(1))
+    val operacionSuma = Suma(primerNumero, Referencia("segundoValor"))
+    val programa = Programa(operacionSuma)
+
+    val interprete = new Interprete()
+
+    assert(interprete.ejecutar(programa) == Numero(5))
+  }
+
+
+  "un variable" should "poderse instanciarse y luego inicializarse" in {
+    val variable1 = Variable("altura") // sin inicializar
+    assert(variable1.valor == None)
+
+    Asignar(Referencia("altura"), Numero(172))
+    assert(variable1.valor == Some(Numero(172)))
+  }
+
+  "el chequeador" should "detectar que hay variables duplicadas" in {
+    val variable1 = Variable("altura", Numero(200))
+    val variable2 = Variable("altura", Numero(400))
+    val programa = Programa(Division(Referencia("altura"), Numero(2)))
+
+
+    val reglaVariableDuplicada = new VariableDuplicada
+    val chequeador = new Chequeador(programa, List(reglaVariableDuplicada))
+
+    assert(chequeador.chequear == List(Problema("Variables Duplicadas", Error(), variable2)))
+  }
+
+  "el chequeador" should "detectar que hay variables no declaradas" in {
+    Referencias.reiniciar()
+    val referencia = Referencia("altura")
+    val programa = Programa(Division(referencia, Numero(2)))
+
+
+    val reglaVariableNoDeclarada = new VariableNoDeclarada
+    val chequeador = new Chequeador(programa, List(reglaVariableNoDeclarada))
+
+    assert(chequeador.chequear == List(Problema("Variables no declarada", Error(), referencia)))
+  }
+
+  "el chequeador" should "detectar que hay variables declaradas no usadas" in {
+    Referencias.reiniciar()
+    val referencia = Referencia("hola")
+    val variable1 = Variable("hola", Numero(10))
+    val variable2 = Variable("chau", Numero(5))
+    val programa = Programa(Division(referencia, Numero(2)))
+
+
+    val reglaVariableDeclaradaNoUsada = new VariableDeclaradaNoUsada
+    val chequeador = new Chequeador(programa, List(reglaVariableDeclaradaNoUsada))
+
+    assert(chequeador.chequear == List(Problema("Variables declaradas no usadas", Advertencia(), variable2)))
+  }
+
+  "el chequeador" should "detectar que hay variables usadas no asignadas" in {
+    Referencias.reiniciar()
+    val referencia = Referencia("hola")
+    val variable1 = Variable("hola")
+    val variable2 = Variable("chau", Numero(5))
+    val programa = Programa(Division(referencia, Numero(2)))
+
+
+    val reglaVariableUsadaNoAsignada = new VariableUsadaNoAsignada
+    val chequeador = new Chequeador(programa, List(reglaVariableUsadaNoAsignada))
+
+    assert(chequeador.chequear == List(Problema("Variables usadas no asignadas", Error(), variable1)))
+  }
+
+
+  "el chequeador" should "detectar que no hay instruccion Retornar en la funcion" in {
+    val funcion = Funcion("suma", List(Parametro("a"), Parametro("b")), List())
+    val reglaDebeRetornarAlgo = new DebeRetornarAlgo
+    val programa = Programa(funcion)
+    val chequeador = new Chequeador(programa, List(reglaDebeRetornarAlgo))
+
+    //assert(!List().exists(p => p.isInstanceOf[Retornar]))
+    assert(chequeador.chequear == List(Problema("La funcion no tiene retorno", Error(), funcion)))
+  }
+
+  "el chequeador" should "detectar que pese a haber instrucciones no Retorna nada" in {
+    val funcion = Funcion("suma", List(Parametro("a"), Parametro("b")), List(Suma(Numero(1), Numero(2))))
+    val reglaDebeRetornarAlgo = new DebeRetornarAlgo
+    val programa = Programa(funcion)
+    val chequeador = new Chequeador(programa, List(reglaDebeRetornarAlgo))
+
+    //assert(!List().exists(p => p.isInstanceOf[Retornar]))
+    assert(chequeador.chequear == List(Problema("La funcion no tiene retorno", Error(), funcion)))
+  }
+
+  "el chequeador" should "detectar que hay instrucciones despues del retorno" in {
+    val funcion = Funcion("FuncionTest", List(Parametro("a"), Parametro("b")), List(Retornar(Suma(Numero(1), Numero(1))), Resta(Numero(4), Numero(1))))
+    val reglaRetornoSiempreUltimo = new RetornoSiempreUltimo
+    val programa = Programa(funcion)
+    val chequeador = new Chequeador(programa, List(reglaRetornoSiempreUltimo))
+
+    assert(chequeador.chequear == List(Problema("La funcion posee instrucciones despues del retorno", Error(), funcion)))
+  }
+
+  "el chequeador" should "detectar nombres de parametros duplicados" in {
+    val funcion = Funcion("FuncionTest", List(Parametro("a"), Parametro("a")), List())
+    val reglaNombreParametrosDuplicados = new NombreParametrosDuplicados
+    val programa = Programa(funcion)
+    val chequeador = new Chequeador(programa, List(reglaNombreParametrosDuplicados))
+
+    assert(chequeador.chequear == List(Problema("nombres de parametros duplicados", Error(), Parametro("a"))))
+  }
+
 }
